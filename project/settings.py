4178d55be90f4f0a4200f76363233ce6f1af0b1f
oZerro <leha.normatov@mail.ru>
2023-07-14 18:33:26 +0300

    Added DB to the environment variable name

--- project/settings.py
+++ project/settings.py
@@ -8,11 +8,11 @@ env.read_env()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': env.str("HOST"),
-        'PORT': env.str("PORT"),
-        'NAME': env.str("NAME"),
-        'USER': env.str("USER"),
-        'PASSWORD': env.str("PASSWORD"),
+        'HOST': env.str("DB_HOST"),
+        'PORT': env.str("DB_PORT"),
+        'NAME': env.str("DB_NAME"),
+        'USER': env.str("DB_USER"),
+        'PASSWORD': env.str("DB_PASSWORD"),
     }
 }
 
